98eb7377774707e411dced708d7c049606998c0f
Jun48714 <junior.matias@unas.edu.pe>
2026-01-29 21:05:22 -0500

    uodate back

--- obj/Debug/net8.0/backendORCinverisones.AssemblyInfo.cs
+++ obj/Debug/net8.0/backendORCinverisones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendORCinverisones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42df62c248cfec0c2bdc912b951e6133b290caa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614c5f0e8ad345f65b6e67cf3a60fd29051a5142")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendORCinverisones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendORCinverisones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
